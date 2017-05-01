92028bda2d49892c78add082bb487a7bf2eff499
deathcod <chinmay.rakshit@gmail.com>
2017-05-01 20:54:10 +0530

    new change

--- ALEXA_skill/publish.sh
+++ ALEXA_skill/publish.sh
@@ -1,7 +1,21 @@
+#!/bin/sh
+
+
 rm xyz.zip
+
+# getting all the packages required
+
+PACKAGE_PATH="Desktop/college_work/my_interviews_and_hackathons/ALEXA_skill/myenv/lib/python3.4/site-packages"
+ZIP_PATH="../../../../xyz.zip"
+
+
 cd 
-cd Desktop/college_work/my_interviews_and_hackathons/ALEXA_skill/myenv/lib/python3.4/site-packages/botocore/vendored/
-zip -r ../../../../../../xyz.zip requests
+cd "$PACKAGE_PATH/botocore/vendored/"
+zip -r "../../$ZIP_PATH" requests
+
+
+
+#getting the files which are necessary
 cd ../../../../../../
 zip -r xyz.zip database src index.py test
 aws lambda update-function-code --region us-east-1 --function-name schedule_task --zip-file fileb://xyz.zip
