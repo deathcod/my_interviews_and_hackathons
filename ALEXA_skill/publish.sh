bbd1e28f9969dd13a24eda88953c7935c59aa72f
deathcod <chinmay.rakshit@gmail.com>
2017-05-01 18:51:06 +0530

    new change

--- ALEXA_skill/publish.sh
+++ ALEXA_skill/publish.sh
@@ -3,7 +3,7 @@ cd
 cd Desktop/college_work/my_interviews_and_hackathons/ALEXA_skill/myenv/lib/python3.4/site-packages/botocore/vendored/
 zip -r ../../../../../../xyz.zip requests
 cd ../../../../../../
-zip -r xyz.zip database src index.py 
+zip -r xyz.zip database src index.py test
 aws lambda update-function-code --region us-east-1 --function-name schedule_task --zip-file fileb://xyz.zip
 git add --all
 git commit -m "new change"
