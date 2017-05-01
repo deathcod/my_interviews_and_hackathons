6604e15bf818f04b8b0531fe3d3c67e03d030789
deathcod <chinmay.rakshit@gmail.com>
2017-05-01 21:13:29 +0530

    new change

--- ALEXA_skill/publish.sh
+++ ALEXA_skill/publish.sh
@@ -27,10 +27,7 @@ cd ../../../../../../
 zip -r xyz.zip database src index.py test
 
 #invoking aws lambda function 
-aws lambda update-function-code\
-		--region us-east-1\
-		--function-name schedule_task\ 
-		--zip-file fileb://xyz.zip
+aws lambda update-function-code --region us-east-1 --function-name schedule_task --zip-file fileb://xyz.zip
 
 #invoking git the credentials are already saved.
 git add --all
