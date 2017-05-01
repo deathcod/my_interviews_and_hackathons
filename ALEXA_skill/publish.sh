f8cc8aa9a2fd694c996b75b663dede43cc146e17
deathcod <chinmay.rakshit@gmail.com>
2017-05-01 21:08:37 +0530

    new change

--- ALEXA_skill/publish.sh
+++ ALEXA_skill/publish.sh
@@ -10,15 +10,27 @@ ZIP_PATH="../../../../xyz.zip"
 
 
 cd 
+
+# adding requests package
 cd "$PACKAGE_PATH/botocore/vendored/"
 zip -r "../../$ZIP_PATH" requests
 
+# adding lxml package
+cd "$PACKAGE_PATH/lxml-3.7.3/src/"
+zip -r "../../$ZIP_PATH" lxml
 
 
 #getting the files which are necessary
 cd ../../../../../../
 zip -r xyz.zip database src index.py test
-aws lambda update-function-code --region us-east-1 --function-name schedule_task --zip-file fileb://xyz.zip
+
+#invoking aws lambda function 
+aws lambda update-function-code 
+		--region us-east-1 
+		--function-name schedule_task 
+		--zip-file fileb://xyz.zip
+
+#invoking git the credentials are already saved.
 git add --all
 git commit -m "new change"
 git push origin master
